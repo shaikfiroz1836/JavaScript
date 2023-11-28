f3ac9ed67dd820b39b769d14a854063a80db03f6
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2023-11-28 09:38:29 +0530

    Test

--- String concat/string.js
+++ String concat/string.js
@@ -2,3 +2,8 @@ function name(Name){
     document.write("Hi my name is " + Name + " and i am learning Javascript");
 }
 name("Firoz");
+
+function name(bit){
+    document.write("Hi my name is " + bit + " and i am learning Javascript");
+}
+name("Shaik");
