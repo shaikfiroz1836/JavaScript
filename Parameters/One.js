a05beaed60a19451d02b1a08aca7965e37fc0305
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2023-12-12 22:05:55 +0530

    tst

--- Parameters/One.js
+++ Parameters/One.js
@@ -4,14 +4,18 @@ class One{
     acc_Bal=0;
 
     open_Account(){}
-    deposit_Amount(){}
+    deposit_Amount(amount){
+        this.acc_Bal=this.acc_Bal+amount
+    }
     withdrawl(){}
-    get_Bal(){}
+    get_Bal(){
+        return this.acc_Bal
+    }
 }
 let a1 = new One();
 console.log(a1)
 a1.open_Account();
-a1.deposit_Amount()
+a1.deposit_Amount(500000)
 a1.withdrawl()
 a1.get_Bal()
-console.log(a1)
+console.log(a1.get_Bal())
