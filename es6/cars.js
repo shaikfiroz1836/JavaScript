1944532424bcf02f98c61f35a13904dcd20f9fc1
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2023-12-15 22:32:04 +0530

    Tst

---
+++ es6/cars.js
@@ -0,0 +1,17 @@
+let cars = [{brand:"maruthi", model:'swift',price:'800000',color:"white"},
+    {brand:"tata", model:'nexon',price:'1800000',color:"pink"},
+    {brand:"maruthi", model:'alto',price:'300000',color:"black"},
+    {brand:"tata", model:'harrier',price:'2800000',color:"white"},
+    {brand:"mahindra", model:'xuv300',price:'900000',color:"white"},
+    {brand:"RR", model:'EVOQUE',price:'5800000',color:"red"},
+    {brand:"Toyota", model:'Fortuner',price:'5400000',color:"white"},
+    {brand:"maruthi", model:'brezza',price:'1400000',color:"white"},
+    {brand:"tata", model:'tiago',price:'600000',color:"yellow"},
+    {brand:"maruthi", model:'grand vitara',price:'1500000',color:"white"},
+    {brand:"ford", model:'eco',price:'1100000',color:"white"}
+]
+
+let new_cars = cars.filter((car)=>{
+    return car.color=="white"&& car.brand!="maruthi"
+})
+console.log(new_cars)
