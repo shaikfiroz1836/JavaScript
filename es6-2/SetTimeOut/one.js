cb0c569f0d45be87c399879ac76f0d96087d99b0
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2023-12-20 22:06:39 +0530

    Async

---
+++ es6-2/SetTimeOut/one.js
@@ -0,0 +1,15 @@
+setTimeout(()=>{
+    console.log("Request 1")
+},1000)
+setTimeout(()=>{
+    console.log("Request 2")
+},4000)
+setTimeout(()=>{
+    console.log("Request 3")
+},8000)
+setTimeout(()=>{
+    console.log("Request 4")
+},11000)
+setTimeout(()=>{
+    console.log("Request 5")
+},30000)
