c1924e66aec69bc0a8459f2f04409eb66baf73ef
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2024-03-27 11:09:31 +0530

    tst

---
+++ Calculator/style.js
@@ -0,0 +1,16 @@
+let string='';
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (event)=>{
+        if(event.target.innerHTML== '='){
+            string = eval(string);
+            document.querySelector('input').value=string;
+        }
+        else{
+            console.log(event.target);
+        string = string+event.target.innerHTML;
+        document.querySelector('input').value=string;
+        }
+        
+    })
+})
