e207dd7b2ba4e51dc61b88024210f3c72c9af0ad
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2024-04-04 13:10:10 +0530

    tst

--- Img to str/Img2Str-1/one.js
+++ Img to str/Img2Str-1/one.js
@@ -1,10 +1,10 @@
 function fileImage(){
     document.getElementById('Image').addEventListener('change', (event) => {
-        const img = event.target.files[0]; // corrected to event.target.files[0]
+        const img = event.target.files[0]; 
         const reader = new FileReader();
 
         reader.addEventListener("load", (event) => {
-            console.log(event); // corrected to console.log(event)
+            console.log(event); 
         });
 
         reader.readAsDataURL(img);
---
+++ Img to str/Img2Str-2/one.js
@@ -0,0 +1,22 @@
+function ImgStr(){
+    // alert("GA")
+    let imageFile=document.getElementById('ImgFile').files[0];
+    let TA = document.getElementById('abc')
+    let reader = new FileReader();
+    reader.readAsDataURL(imageFile); 
+    reader.addEventListener( "load",()=>{
+        console.log(reader.result)
+        TA.innerText=reader.result;
+        globalStr=reader.result;
+    })
+}
+
+var globalStr=''
+
+function Image(){
+    // alert('GN')
+    let ImgRef = document.getElementById('xyz')
+    ImgRef.innerHTML=globalStr;
+    globalStr=reader.result;
+
+}
