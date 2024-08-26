63f9923ec0a4477e4425de6b727738bed3d85069
Shaik.Mahammad Khaja Firoz <91871437+shaikfiroz1836@users.noreply.github.com>
2024-08-26 22:56:23 +0530

    Update Array.js

--- Array/Array.js
+++ Array/Array.js
@@ -3,6 +3,11 @@ let Emp = {
     name: "Firoz",
     sal: 65000
 };
-
+let emp = {
+    id: 2,
+    name: "Modi",
+    sal: 75000
+};
 console.log(Emp);
+console.log(emp);
 document.write(JSON.stringify(Emp));
